1335227850535969665f25b66e80074ab40620fd
Zubicoa Garijo <rzubicoa@minsait.com>
2026-06-12 12:27:01 +0200

    feat: crear la seccion de la idea

--- src/app/pages/projects/pages/idea/idea.ts
+++ src/app/pages/projects/pages/idea/idea.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { PdfViewer } from '../../../../shared/components/pdf-viewer/pdf-viewer';
 
 @Component({
   selector: 'app-idea',
-  imports: [],
+  imports: [RouterLink, TranslateModule, PdfViewer],
   templateUrl: './idea.html',
   styleUrl: './idea.scss',
 })
-export class Idea {}
+export class Idea {
+
+  public readonly pdfPath1 = '/documents/idea/idea.pdf';
+}
+  
