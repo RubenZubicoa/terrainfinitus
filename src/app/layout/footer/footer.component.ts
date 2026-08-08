df9f3f0237399aa452faf85b580fd91cd9e4ab3c
Ruben <rubenzubicoatic@gmail.com>
2026-08-08 08:50:55 +0200

    feat: quitar botones del pie de pagina

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink, TranslateModule],
+  imports: [TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
